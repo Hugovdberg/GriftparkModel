7356411f9ce6970a5f66f55d2aec708d364ec90a
Hugo van den Berg <hugo@tbdwebdesign.nl>
2019-06-02 22:18:46 +0200

    Updated map

--- Griftpark_Model.py
+++ Griftpark_Model.py
@@ -472,28 +472,28 @@ conc2 = conc_file2.get_alldata()
 for icomp, comp in enumerate([conc1, conc2]):
     comp_name = mt.btn.species_names[icomp]
     for ilay in range(mf.dis.nlay):
-fig, axs = plt.subplots(ncols=2, figsize=(24, 16))
-ax, ax2 = axs
+        fig, axs = plt.subplots(ncols=2, figsize=(24, 16))
+        ax, ax2 = axs
         ax.set_title(f"Original concentration {comp_name} in layer {ilay+1}")
         ax2.set_title(f"Final concentration {comp_name} in layer {ilay+1}")
-ax.set_aspect("equal")
-ax2.set_aspect("equal")
-extent = None
-extent = [137000, 137400, 456525, 457325]  # Wall
-pmv = flopy.plot.PlotMapView(model=mf, ax=ax, layer=ilay, extent=extent)
-pmv_conc = flopy.plot.PlotMapView(model=mf, ax=ax2, layer=ilay, extent=extent)
-for segment in wall_segments:
-    v = ax.plot(*segment, color="grey")
-# c = pmv.plot_array(heads, alpha=0.5, masked_values=[-999.99])
+        ax.set_aspect("equal")
+        ax2.set_aspect("equal")
+        extent = None
+        extent = [137000, 137400, 456525, 457325]  # Wall
+        pmv = flopy.plot.PlotMapView(model=mf, ax=ax, layer=ilay, extent=extent)
+        pmv_conc = flopy.plot.PlotMapView(model=mf, ax=ax2, layer=ilay, extent=extent)
+        for segment in wall_segments:
+            v = ax.plot(*segment, color="grey")
+        # c = pmv.plot_array(heads, alpha=0.5, masked_values=[-999.99])
         c = pmv.plot_array(
             comp[0], masked_values=[comp.max()], alpha=0.5, vmin=0, vmax=1
         )
-plt.colorbar(c, ax=ax)
-pmv.plot_discharge(
-    frf=frf, fff=fff, flf=flf, head=heads, color="#ff0000", istep=3, jstep=3
-)
-pmv.plot_bc("WEL", plotAll=True)
-pmv.plot_shapefile("data/resistive_wall.shp", alpha=1, facecolor="None")
+        plt.colorbar(c, ax=ax)
+        pmv.plot_discharge(
+            frf=frf, fff=fff, flf=flf, head=heads, color="#ff0000", istep=3, jstep=3
+        )
+        pmv.plot_bc("WEL", plotAll=True)
+        pmv.plot_shapefile("data/resistive_wall.shp", alpha=1, facecolor="None")
         # pmv.plot_shapefile("data/PAK/PAK_0_5.shp", alpha=0.5, facecolor="red")
         pmv.plot_shapefile(
             "data/location_wells.shp", radius=1, edgecolor="blue", facecolor="b"
@@ -502,13 +502,13 @@ pmv.plot_shapefile("data/resistive_wall.shp", alpha=1, facecolor="None")
         c = pmv_conc.plot_array(
             comp[-1], masked_values=[comp.max()], alpha=0.5, vmin=0, vmax=1
         )
-plt.colorbar(c, ax=ax2)
-for segment in wall_segments:
-    v = ax2.plot(*segment, color="grey")
+        plt.colorbar(c, ax=ax2)
+        for segment in wall_segments:
+            v = ax2.plot(*segment, color="grey")
 
-fig.tight_layout()
+        fig.tight_layout()
         fig.savefig(model_output_dir / f"compare_concentrations_{comp_name}_{ilay}.png")
-plt.close(fig)
+        plt.close(fig)
 
 xs_lines = {
     "A-A'": [[137215, 457200], [137215, 456600]],
@@ -524,26 +524,26 @@ for icomp, comp in enumerate([conc1, conc2]):
         ax2.set_title(f"Final concentration {comp_name} on transect {line_title}")
         # ax.set_aspect(3)
         # ax2.set_aspect(3)
-# row=72, column=51
+        # row=72, column=51
         # xs_line = wells
         # xs_line = [[137215, 457200], [137215, 456600]]
-pxs = flopy.plot.PlotCrossSection(
+        pxs = flopy.plot.PlotCrossSection(
             model=mf, ax=ax, line={"line": xs_line}  # , extent=(600, 800, -80, 0)
         )
         pxs2 = flopy.plot.PlotCrossSection(
             model=mf, ax=ax2, line={"line": xs_line}  # , extent=(600, 800, -80, 0)
-)
-pxs.plot_grid(linewidths=0.5, alpha=0.5)
+        )
+        pxs.plot_grid(linewidths=0.5, alpha=0.5)
         pxs2.plot_grid(linewidths=0.5, alpha=0.5)
         c = pxs.plot_array(
             comp[0], head=heads, masked_values=[conc2.max()], vmin=0, vmax=1
         )
-plt.colorbar(c, ax=ax)
+        plt.colorbar(c, ax=ax)
         c = pxs2.plot_array(
             comp[-1], head=heads, masked_values=[conc2.max()], vmin=0, vmax=0.5
         )  #
         plt.colorbar(c, ax=ax2)
-# pxs.plot_ibound(color_noflow="#aaaaaa", head=heads)
+        # pxs.plot_ibound(color_noflow="#aaaaaa", head=heads)
         # pxs.plot_discharge(
         #     frf=frf,
         #     fff=fff,
@@ -554,9 +554,9 @@ plt.colorbar(c, ax=ax)
         #     hstep=3,
         #     kstep=1,
         # )
-fig.tight_layout()
+        fig.tight_layout()
         fig.savefig(model_output_dir / f"crosssection_{comp_name}_{line_title[0]}.png")
-plt.close(fig)
+        plt.close(fig)
 
 with fiona.open(
     "output/crosssections.shp",
